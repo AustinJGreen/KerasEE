5afe346f3ef938d06d004311bfdd858ae516caff
austi <austin.green@outlook.com>
2019-02-16 01:17:31 -0800

    Switch to normal encoding, retrain

--- src/translator.py
+++ src/translator.py
@@ -5,7 +5,6 @@ import numpy as np
 from keras.layers.convolutional import Conv2D
 from keras.layers.core import Activation
 from keras.models import Sequential, load_model
-from keras.optimizers import Adam
 
 import utils
 from loadworker import load_worlds_with_minimaps
@@ -28,7 +27,7 @@ def build_translator(size):
     return model
 
 
-def train(epochs, batch_size, world_count, version_name=None, initial_epoch=0):
+def train(epochs, batch_size, world_count, version_name=None):
     cur_dir = os.getcwd()
     res_dir = os.path.abspath(os.path.join(cur_dir, '..', 'res'))
     all_models_dir = os.path.abspath(os.path.join(cur_dir, '..', 'models'))
@@ -52,16 +51,12 @@ def train(epochs, batch_size, world_count, version_name=None, initial_epoch=0):
     print('Loading minimap values...')
     minimap_values = utils.load_minimap_values(res_dir)
 
-    print('Loading block images...')
-    block_images = utils.load_block_images(res_dir)
-
     print('Loading encoding dictionaries...')
-    block_forward, block_backward = utils.load_encoding_dict(res_dir, 'blocks_colored')
+    block_forward, block_backward = utils.load_encoding_dict(res_dir, 'blocks_optimized')
 
     print('Building model from scratch...')
-    optim = Adam(lr=0.0001)
     translator = build_translator(112)
-    translator.compile(optimizer='sgd', loss='mse')
+    translator.compile(optimizer='adam', loss='mse')
 
     print('Loading worlds...')
     x_train, y_train = load_worlds_with_minimaps(world_count, '%s\\worlds\\' % res_dir, (112, 112), block_forward,
@@ -71,12 +66,12 @@ def train(epochs, batch_size, world_count, version_name=None, initial_epoch=0):
                                                          save_best_only=True, save_weights_only=False, mode='min',
                                                          period=1, monitor='loss')
 
-    # Create callback for automatically saving lastest model so training can be resumed. Saves every epoch
+    # Create callback for automatically saving latest model so training can be resumed. Saves every epoch
     latest_h5_callback = keras.callbacks.ModelCheckpoint('%s\\latest.h5' % model_save_dir, verbose=0,
                                                          save_best_only=False,
                                                          save_weights_only=False, mode='auto', period=1)
 
-    # Create callback for automatically saving lastest weights so training can be resumed. Saves every epoch
+    # Create callback for automatically saving latest weights so training can be resumed. Saves every epoch
     latest_weights_callback = keras.callbacks.ModelCheckpoint('%s\\latest.weights' % model_save_dir, verbose=0,
                                                               save_best_only=False,
                                                               save_weights_only=True, mode='auto', period=1)
@@ -86,7 +81,6 @@ def train(epochs, batch_size, world_count, version_name=None, initial_epoch=0):
                                               write_grads=True)
 
     callback_list = [latest_h5_callback, latest_weights_callback, best_loss_callback, tb_callback]
-
     translator.fit(x_train, y_train, batch_size, epochs, callbacks=callback_list)
 
 
@@ -111,14 +105,13 @@ def test(version_name, samples):
     block_forward, block_backward = utils.load_encoding_dict(res_dir, 'blocks_optimized')
 
     print('Loading model...')
-    translator = load_model('%s\\latest.h5' % model_save_dir)
+    translator = load_model('%s\\best_loss.h5' % model_save_dir)
 
     print('Loading worlds...')
-    x_train, y_train = load_worlds_with_minimaps(10, '%s\\worlds\\' % res_dir, (112, 112), block_forward,
+    x_train, y_train = load_worlds_with_minimaps(samples, '%s\\worlds\\' % res_dir, (112, 112), block_forward,
                                                  minimap_values)
 
-    world_count = x_train.shape[0]
-    for i in range(world_count):
+    for i in range(samples):
         utils.save_world_preview(block_images, utils.decode_world_sigmoid(block_backward, x_train[i]),
                                  '%s\\world%s.png' % (tests_dir, i))
         utils.save_rgb_map(utils.decode_world_minimap(y_train[i]), '%s\\truth%s.png' % (tests_dir, i))
@@ -128,8 +121,8 @@ def test(version_name, samples):
 
 
 def main():
-    train(epochs=50, batch_size=1, world_count=100)
-    # test('ver4', 10)
+    train(epochs=50, batch_size=1, world_count=10000)
+    # test('ver14', 15)
 
 
 if __name__ == '__main__':
