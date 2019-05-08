1af0cfb3475515d5cf7a662a6b19b4dbb6c28cda
austi <austin.green@outlook.com>
2019-05-07 22:53:14 -0700

    Oops fix ps zip behavior

--- build_dk_zip.cmd
+++ build_dk_zip.cmd
@@ -1,4 +1,4 @@
-powershell.exe -nologo -noprofile -command "& { Compress-Archive -Path .\eedream\*.* -CompressionLevel Optimal -DestinationPath .\eedream.zip }"
+powershell.exe -nologo -noprofile -command "& { Compress-Archive -Path .\eedream\ -CompressionLevel Optimal -DestinationPath .\eedream.zip }"
 rd /s /q .\eedream\
 if not exist .\eedream\ rd /s /q .\eedream\
 exit
