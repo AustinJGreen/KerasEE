b672a736a0242ceb694ee49753e54250dd498411
austi <austin.green@outlook.com>
2019-05-07 20:00:52 -0700

    Update build kit

--- build_dream_kit.cmd
+++ build_dream_kit.cmd
@@ -1,4 +1,6 @@
+rmdir /s /q .\eedream\
 mkdir eedream
+xcopy src\playerio\* eedream\playerio\
 xcopy src\blocks.py eedream\
 xcopy src\utils.py eedream\
 xcopy src\dream.py eedream\
@@ -7,14 +9,18 @@ xcopy res\block_colors.txt eedream\res\
 xcopy res\blocks_optimized.txt eedream\res\
 copy models\pro_classifier\ver38\models\latest.h5 eedream\model.h5
 cd eedream
-echo python dream.py --world myworld.eelvl >> do_dream.cmd
-echo Step 1. Download eelvl from EE. >> README.txt
+echo python dream.py --world myworld.eelvl --resources .\res\ >> do_dream.cmd
+echo Before starting, make sure you have python 3.6 or greater installed >> README.txt
+echo Step 1. Download eelvl from EE >> README.txt
 echo Step 2. Put file in same directory as dream.py >> README.txt
 echo Step 3. Edit do_dream.cmd and replace myworld.eelvl with the name of your level >> README.txt
-echo Or, you can run python dream.py --world myworld.eelvl >> README.txt
-echo *** Make sure you have python 3.6 installed. *** >> README.txt
 echo: >> README.txt
-echo Pro: Step 4. Open up dream.py and play with parameters. Change params (which change settings) for visual effects >> README.txt
+echo Make sure your level name does not have any spaces in it, or if it does, wrap it with quotes >> README.txt
+echo Example: "Nostalgia - cheeseeater - PWr29a2r1lcEI.eelvl" >> README.txt
+echo: >> README.txt
+echo Profit: Step 4. Open up dream.py and play with parameters. Change params (which change settings) for visual effects >> README.txt
 echo and play around with step, num_octave, octave_scale, iterations and max_loss. >> README.txt
 echo: >> README.txt
 echo - ugp >> README.txt
+powershell.exe -nologo -noprofile -command "& { Compress-Archive -Path .\eedream\* -CompressionLevel Optimal -DestinationPath .\eedream.zip }"
+rmdir /s .\eedream\
